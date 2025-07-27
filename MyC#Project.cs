704e909cb6adb10ab77035cbd942b5e30dcbc888
JeetP35 <jeetpadhya35@gmail.com>
2025-07-27 15:23:40 -0600

    Create MyC#Project.cs

---
+++ MyC#Project.cs
@@ -0,0 +1,10 @@
+namespace Practice&Learning
+{
+    public class MyCSharpProject
+    {
+        public void DisplayMessage()
+        {
+            System.Console.WriteLine("Hello, this is my C# project!");
+        }
+    }
+}
