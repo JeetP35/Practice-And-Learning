ea797a61bd9c6e7a1de6f5c1cd358351daff4719
JeetP35 <jeetpadhya35@gmail.com>
2025-12-16 16:36:41 -0700

    Create 18.py

---
+++ BootDevPython/2Variables/18.py
@@ -0,0 +1,38 @@
+'''
+Spellbook
+As you progress through Boot.dev, you might find yourself wanting to:
+
+Take notes
+Review previous lessons or concepts you already learned
+See more examples of a concept
+See an example of some syntax you already learned
+Your spellbook solves these problems.
+
+It's a powerful tool that lets you search any lesson on Boot.dev. In addition, as you complete lessons, you'll automatically unlock spellbook pages - handy reference pages that can also be searched. So, if you know what you're looking for, use your spellbook before chatting with Boots, using it never costs items or XP!
+
+Assignment
+Competitive Fantasy Quest players – Ballan's Ballers – have won all their games in the elimination round of the tournament. They're now in the finals! Calculate the average score of their 4 games and store it in the average_score variable.
+
+The average (or "mean") is calculated by adding up all the numbers and dividing by how many numbers there are. Which means first we need to add up all the scores, then divide by 4.
+
+Let's use our spellbook to recall the syntax we need:
+
+Find and open the spellbook.
+Look at the bottom-right of the lesson text, you'll see a toggle to switch from Boots to your spellbook. Flip the switch to open your spellbook.
+On mobile, it's the last tab at the top of the screen.
+Search for the concept you'll need for this lesson, try "python math".
+Complete the lesson using your spellbook as a reference
+'''
+
+game_one_score = 97
+game_two_score = 92
+game_three_score = 106
+game_four_score = 105
+
+# Don't touch above this line
+
+average_score = (game_one_score + game_two_score + game_three_score + game_four_score) / 4
+
+# Don't touch below this line
+
+print(round(average_score))
