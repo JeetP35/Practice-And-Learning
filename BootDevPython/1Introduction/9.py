3b2e30bd4ee9c0880b8556650728032d1d50fe99
JeetP35 <jeetpadhya35@gmail.com>
2025-12-16 15:25:23 -0700

    Create 9.py

---
+++ BootDevPython/1Introduction/9.py
@@ -0,0 +1,18 @@
+'''
+What does syntax mean?
+
+
+
+The rules for valid code in a programming language
+
+
+Fast code
+
+
+Code that produces the correct output
+
+
+Code in the browser
+
+Answer : The rules for valid code in a programming language
+'''
