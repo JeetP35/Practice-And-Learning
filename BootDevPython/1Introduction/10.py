5231ec25be9bc56de29cd8af785ae169a0ec76d1
JeetP35 <jeetpadhya35@gmail.com>
2025-12-16 16:36:17 -0700

    Update 10.py

---
+++ BootDevPython/1Introduction/10.py
@@ -0,0 +1,18 @@
+'''
+What happens if you try to run code with invalid syntax?
+
+
+
+Your network connection will drop
+
+
+Your dreams will wither and die, alongside your last few hopes and aspirations
+
+
+You'll get an error message from the Python interpreter and the code won't execute
+
+
+The computer will freeze
+
+Correct Answer : You'll get an error message from the Python interpreter and the code won't execute
+'''
