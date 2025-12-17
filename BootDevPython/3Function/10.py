d9c1b0c76c12cb70e0df6e2fcd3a917b5ffc5224
JeetP35 <jeetpadhya35@gmail.com>
2025-12-17 11:12:21 -0700

    Update 10.py

---
+++ BootDevPython/3Function/10.py
@@ -0,0 +1,46 @@
+'''
+Boots Interview
+It's time for your first Boots interview lesson!
+
+In these lessons, Boots turns on you! He becomes your interviewer rather than your tutor. Answer his open-ended question or follow his instructions. The chat will continue until he’s satisfied that your responses meet the acceptance criteria.
+
+Treat it like a real interview:
+
+You're allowed to ask clarifying questions without penalty (it's actually encouraged)!
+You don't have to answer in one shot, have a back-and-forth conversation!
+Boots will respond in one of three ways:
+
+If you ask a question or give a partially correct or inconclusive answer, there's no penalty and Boots will ask a follow-up question or clarify the instructions
+If you say something that's wrong or off-topic, you'll lose an armor/spree and Boots will try to redirect you
+Once you satisfy the acceptance criteria, Boots will pass you
+Assignment
+Reference the following code snippet to answer Boots' question:
+
+def calculate_damage(opening_attack, core_damage, finishing_move):
+    total = opening_attack + core_damage + finishing_move
+    return total
+
+
+first_damage = calculate_damage(10, 20, 30)
+second_damage = calculate_damage(5, 10, 15)
+third_damage = calculate_damage(50, 60, 70)
+print(first_damage)
+print(second_damage)
+print(third_damage)
+'''
+
+'''
+Interview with Boots
+Boots
+The calculate_damage function is called 3 times in the example. When the total is returned, which variables are used to store it?
+
+
+Lesson Complete!
+You've successfully addressed all the points Boots wanted to hear.
+
+Hide Solution
+
+Boots is expecting you to satisfy the acceptance criteria:
+
+The first call to `calculate_damage` stores the result in `first_damage`. The second call to `calculate_damage` stores the result in `second_damage`. The third call to `calculate_damage` stores the result in `third_damage`.
+'''
