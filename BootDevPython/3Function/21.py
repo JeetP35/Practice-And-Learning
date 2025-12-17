7636b51da9ec5482e3345d6a399e59fd26a1807d
JeetP35 <jeetpadhya35@gmail.com>
2025-12-17 11:12:31 -0700

    Update 21.py

---
+++ BootDevPython/3Function/21.py
@@ -0,0 +1,43 @@
+'''
+Archmage
+Congratulations on making it to the end of the functions chapter!
+
+This is one of the hardest chapters in the Python course! Although dictionaries are no walk in the park either...
+
+You've probably already figured out that:
+
+Chests contain gems and items
+Gems can buy items in the shop
+Items help you earn achievements and XP
+XP levels you up, and higher levels unlock new roles
+So why should you care about your role?
+
+You've probably almost earned (or maybe you already have) the Apprentice role. You can see your role at the top right of your screen. If you don't have a role yet, don't worry, you'll get Apprentice at level 10.
+
+When you hit level 100, you'll unlock the final role, Archmage. It's kind of a big deal. When you hit archmage:
+
+You'll have learned a ton, built many real-world projects, and should be starting your job search
+You'll have unlocked all the private role-based chat channels in our Discord
+You'll get a physical archmage coin in the mail as a token of your hard work
+'''
+
+
+'''
+Which role is highest in the Boot.dev hierarchy?
+
+
+
+Pupil
+
+
+Mage
+
+
+Archmage
+
+
+Apprentice
+
+
+Correct Answer : Archmage
+'''
