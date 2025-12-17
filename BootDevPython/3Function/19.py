b91b7d2f4c872beef181e2e709b894b0228df32c
JeetP35 <jeetpadhya35@gmail.com>
2025-12-17 11:12:28 -0700

    Update 19.py

---
+++ BootDevPython/3Function/19.py
@@ -0,0 +1,28 @@
+'''
+The Training Grounds
+This lesson has nothing to do with Python: I just want to show you a resource you have at your disposal for practicing your skills.
+
+The Training Grounds ("Training" in the top nav) is where you can practice infinite coding challenges and interview questions. Our courses don't repeat themselves very often... our expectation is that once you learn a concept and complete its lesson, you're ready to move to the next concept.
+
+If you find yourself wanting more practice before moving on, you can head over to the Training Grounds at any time and grind some challenges.
+
+Challenges in the training grounds are personalized to you and what you've been working on recently, but you can also search the existing database of challenges to find specific topics.
+
+If you're not a paying member you've been awarded 25 free challenges. Once you upgrade to a paid membership, you'll have unlimited challenges.
+'''
+
+
+'''
+The Training Grounds are ____
+
+
+
+Boots' nickname for his favorite fishing spot
+
+
+A place to practice what you've already learned with personalized challenges before forging ahead in the course
+
+
+
+Correct Answer: A place to practice what you've already learned with personalized challenges before forging ahead in the course
+'''
