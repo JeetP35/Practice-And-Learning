51fa787f2578ac95edca7ef5d81e34d3c50e162d
JeetP35 <jeetpadhya35@gmail.com>
2025-12-17 11:12:23 -0700

    Update 13.py

---
+++ BootDevPython/3Function/13.py
@@ -0,0 +1,42 @@
+'''
+None Return
+When no return value is specified in a function, it will automatically return None. For example, maybe it's a function that prints some text to the console, but doesn't explicitly return a value. The following code snippets all return the same value, None:
+
+def my_func():
+    print("I do nothing")
+    return None
+
+def my_func():
+    print("I do nothing")
+    return
+
+def my_func():
+    print("I do nothing")
+'''
+
+
+'''
+What's the difference between printing a value and returning it?
+
+
+
+They are the same
+
+
+Print: Shows a value in the console.
+Return: Makes a value available to the caller of the function and shows it in the console
+
+
+Print: Shows a value in the console.
+Return: Makes a value available to the caller of the function
+
+
+Print: Makes a value available to the caller of the function.
+Return: Shows a value in the console
+
+
+Correct Answer : 
+
+    Print: Shows a value in the console.
+    Return: Makes a value available to the caller of the function
+'''
