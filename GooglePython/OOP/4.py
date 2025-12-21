2d687c0e087ca06ec4adbb8bdeb97ec63413513f
JeetP35 <jeetpadhya35@gmail.com>
2025-12-21 16:10:32 -0700

    Create 4.py

---
+++ GooglePython/OOP/4.py
@@ -0,0 +1,8 @@
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+
+sigma_jonagold = Apple("red", "sweet")
+print(sigma_jonagold.color)
+print(sigma_jonagold.flavor)
