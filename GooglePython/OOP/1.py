825197b05ea947f71f498cff708f2ff07479f2ba
JeetP35 <jeetpadhya35@gmail.com>
2025-12-21 16:10:25 -0700

    Create 1.py

---
+++ GooglePython/OOP/1.py
@@ -0,0 +1,17 @@
+class apple:
+    color = ""
+    flavor = ""
+    pass
+
+jonagold = apple()
+jonagold.color = "red"
+jonagold.flavor = "sweet"
+
+print(jonagold.color)
+print(jonagold.flavor)
+
+print(jonagold.color.upper())
+
+golden = apple()
+golden.color = "yellow"
+golden.flavor = "soft"
