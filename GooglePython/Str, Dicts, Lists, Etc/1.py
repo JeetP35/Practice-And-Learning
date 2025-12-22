8db672aa65e2b891fb8cbe6ff7abc82b6db7245e
JeetP35 <jeetpadhya35@gmail.com>
2025-12-22 14:35:00 -0700

    Update 1.py

---
+++ GooglePython/Str, Dicts, Lists, Etc/1.py
@@ -0,0 +1,8 @@
+str = "Hello World"
+length = len(str)
+print("Original string:", str)
+print("Length of the string is:", length)
+print("First character:", str[0])
+print("Last character:", str[-1])
+print("Substring (0-5):", str[:5])
+print("Substring (6-end):", str[6:])
